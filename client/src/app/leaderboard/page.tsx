08c9b8ca835bfd23292da409b5a23442987936fb
Khye Jac <lowkhyejac@gmail.com>
2024-06-29 06:16:47 +1000

    did leaderboard stuff

--- client/src/app/leaderboard/page.tsx
+++ client/src/app/leaderboard/page.tsx
@@ -4,16 +4,25 @@ import PlayerCard from '@/components/PlayerCard';// Assuming the correct path is
 
 export default function Leaderboard() {
     return (
-        <div className="w-full h-screen bg-[#ebfbee]">
-            <p className='text-4xl flex justify-center'>Leaderboard</p>
-            <div className='flex flex-row w-full justify-center gap-4'>
-                <p>friends</p>
-                <p>|</p>
-                <p>global</p>
+        <div className="w-screen h-screen bg-[#1CB87C] ">
+            <div className='bg-[rgb(28,184,124)] py-8 pb-4 border-b-1'>
+                <p className='text-4xl font-bold text-white flex justify-center'>Leaderboard</p>
+                <div className='flex flex-row w-full justify-center gap-4'>
+                    <p className='text-white text-2xl underline'>friends</p>
+                    <p className='text-white text-2xl'>|</p>
+                    <p className='text-[#E3E3E3] text-2xl'>global</p>
+                </div>
             </div>
-            <PlayerCard name='Andrew' level='500' petAmount={80} questAmount={120} />
-            <PlayerCard name='Eric' level='200' petAmount={42} questAmount={23} />
-            <PlayerCard name='Jackson' level='10' petAmount={23} questAmount={10} />
+            <div className='bg-white py-2 h-full rounded-t-[30px] overflow-y-auto'>
+                <PlayerCard img="/assets/frogchad.jpg" name='Kj' level='500' petAmount={80} questAmount={120} />
+                <PlayerCard img="/assets/frogmanner.jpg" name='Jerry' level='200' petAmount={42} questAmount={23} />
+                <PlayerCard img="/assets/frogman.jpg" name='Jackson' level='10' petAmount={23} questAmount={10} />
+                <PlayerCard img="/assets/chickboy.jpg" name='Eric' level='2' petAmount={3} questAmount={3} />
+                <PlayerCard img="/assets/kitty.png" name='Andrew' level='1' petAmount={2} questAmount={1} />
+                <PlayerCard img="/assets/kitty.png" name='Andrew' level='1' petAmount={2} questAmount={1} />
+                <PlayerCard img="/assets/kitty.png" name='Andrew' level='1' petAmount={2} questAmount={1} />
+                <PlayerCard img="/assets/kitty.png" name='Andrew' level='1' petAmount={2} questAmount={1} />
+            </div>
         </div>
     )
 }
--- client/src/components/PlayerCard.tsx
+++ client/src/components/PlayerCard.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface PlayerCardProps {
+    img: string;
     name: string;
     level: string;
     petAmount: number;
     questAmount: number;
 }
 
-const PlayerCard: React.FC<PlayerCardProps> = ({ name, level, petAmount, questAmount }) => {   
+const PlayerCard: React.FC<PlayerCardProps> = ({img, name, level, petAmount, questAmount }) => {   
     return (
-        <div className='w-[90%] my-4 ml-6 py-2 flex justify-center bg-[#b2f2bb] border-2 border-black rounded-2xl'>
-            <div className='flex w-full p-2 flex-row gap-4'>
-                <p className='flex flex-[1] justify-center items-center'>IMAGE</p>
+        <div className='w-[90%] my-4 mx-6 flex justify-center bg-[#ffffff] border-2 border-black rounded-2xl shadow-xl'>
+            <div className='flex w-full flex-row gap-4'>
+                <div className='bg-[#1CB87C] p-2 rounded-l-2xl border-black border-r-2'>
+                    <Image src={img} alt="Frogman" width={100} height={100} className='flex justify-center items-center border-2 border-black rounded-full'></Image>
+                </div>
                 <div className='flex flex-[3] flex-col w-full'>
                     <div className='flex flex-row w-full py-1 justify-center'>
-                        <p className='text-2xl flex-1 flex justify-center'>{name}</p>
+                        <p className='text-2xl flex-1 flex justify-items-start'>{name}</p>
                         <p className='text-2xl flex-1 flex justify-center'>Lvl {level}</p>
                     </div>
-                    <div className='flex flex-col w-[90%] bg-[#40c057] p-2 border-2 border-black rounded-xl'>
-                        <p>Pets: {petAmount}/90</p>
-                        <p>Quests: {questAmount}</p>
+                    <div className='flex flex-col w-[90%] bg-[#1CB87C] border-2 border-black rounded-xl'>
+                        <p className='text-white width-full border-b-2 border-black px-2'>Pets: {petAmount}/90</p>
+                        <p className='text-white px-2'>Quests: {questAmount}</p>
                     </div>
                 </div>
             </div>
