e1fb4e090915f5514584452644c585aafff93416
Arnold45202 <kahowang3659@gmail.com>
2024-06-29 01:04:36 +1000

    adding the background page

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,18 @@
+// src/pages/login.tsx
+import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/login.module.css';
+
+const Login = () => {
+  return (
+    <>
+      <Head>
+        <title>Login Page</title>
+      </Head>
+      <div className={styles.loginPage}>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import Link from 'next/link';
 
-export default function Home() {
+const Home = () => {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-10">
-			Filler
+			<h1>Welcome to the Home Page</h1>
+			<nav>
+				<ul>
+					<li><Link href="/login">login </Link></li>
+					<li><Link href="/leaderboard"> leardboard</Link></li>
+				</ul>
+			</nav>
 		</main>
 	);
-}
+};
+
+export default Home;
