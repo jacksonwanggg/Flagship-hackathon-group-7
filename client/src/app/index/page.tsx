15541db72cf8a5ca4dffc982a201b6b282fe99ba
Andrew Zhang <andrewzhang@Andrews-MacBook-Pro.local>
2024-06-29 02:24:21 +1000

    created post cards for socials

--- client/src/app/index/page.tsx
+++ client/src/app/index/page.tsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      Filler
-    </main>
-  );
+import { useState, useEffect } from "react";
+import PostList from "@/components/PostCardList";
+interface Post {
+  userName: string;
+  caption: string;
 }
+
+const fetchPosts = async (): Promise<Post[]> => {
+  const res = await fetch("/assets/posts.json");
+  const posts: Post[] = await res.json();
+  return posts;
+};
+
+const Home: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const fetchedPosts = await fetchPosts();
+      setPosts(fetchedPosts);
+    };
+
+    getPosts();
+  }, []);
+
+  return (
+    <div className="container mx-auto p-4">
+      <PostList posts={posts} />
+    </div>
+  );
+};
+
+export default Home;
