ac85b37d0388a63a8803c18421fe2041b5c24aae
Jerry Hong <jh1012037421@gmail.com>
2024-06-29 01:25:43 +1000

    implemented navbar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import React from "react";
 import Home from "./index/page";
+import { useState } from "react";
 
-export default function Home() {
+export default function Page() {
+	const [activePage, setActivePage] = useState("home");
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-10">
-			<Navbar></Navbar>
+			{/* <Home></Home> */}
+			<Navbar
+				activePage={activePage}
+				setActivePage={setActivePage}></Navbar>
 		</main>
 	);
 }
