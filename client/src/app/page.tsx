764a2e094e11dbfd75bce61ea1ab629926de770d
Jerry Hong <jh1012037421@gmail.com>
2024-06-29 02:16:35 +1000

    imeplemented

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,8 +3,6 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-10">
-			<Navbar></Navbar>
-		</main>
+		<main className="flex min-h-screen flex-col items-center justify-between p-10"></main>
 	);
 }
