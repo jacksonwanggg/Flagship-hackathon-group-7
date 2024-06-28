c21f00456fea841d0021fc822baff2e51aa16b4d
Jerry Hong <jh1012037421@gmail.com>
2024-06-29 01:25:43 +1000

    implemented navbar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 
-export default function Home() {
+export default function Page() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-10"></main>
 	);
