2d0a398af22b487d628bcbad4077052b14a0641c
Jerry Hong <jh1012037421@gmail.com>
2024-06-29 01:05:16 +1000

    handled merge conflicts

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,12 @@
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
 import React from "react";
 import Home from "./index/page";
 
-export default async function Page() {
-  return <Home />;
+export default function Home() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-10">
+			<Navbar></Navbar>
+		</main>
+	);
 }
