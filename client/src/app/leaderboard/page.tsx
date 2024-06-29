601398a26c3ce2dcaa963c1aa1b902195b0db32c
Khye Jac <lowkhyejac@gmail.com>
2024-06-29 19:06:20 +1000

    tried some styling

--- client/src/app/leaderboard/page.tsx
+++ client/src/app/leaderboard/page.tsx
@@ -1,51 +1,94 @@
+"use client";
+
 import React from 'react';
-import Link from 'next/link';
 import PlayerCard from '@/components/PlayerCard';// Assuming the correct path is '../components/PlayerCard/PlayerCard'
+import { useState } from 'react';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+
 
 export default function Leaderboard() {
-    const players = [
-        { img: "/assets/kj.jpg", img1: "/assets/sheepchad.png", img2: "/assets/sharkchad.png", img3: "/assets/giraffechad.png", name: 'Kj', level: 500, petAmount: 80, questAmount: 120 },
-        { img: "/assets/andrew.jpg", img1: "/assets/frogchad.jpg", img2: "/assets/giraffeman.png", img3: "/assets/sheeper.png", name: 'Andrew', level: 230, petAmount: 42, questAmount: 80 },
+    const [tab, setTab] = useState("friends"); 
+
+    const friends = [
+        { img: "/assets/kj.jpg", img1: "/assets/sheepchad.png", img2: "/assets/sharkchad.png", img3: "/assets/giraffechad.png", name: 'Kj', level: 260, petAmount: 80, questAmount: 120 },
+        { img: "/assets/andrew.jpg", img1: "/assets/frogchad.jpg", img2: "/assets/giraffeman.png", img3: "/assets/sheeper.png", name: 'Andrew', level: 230, petAmount: 42, questAmount: 102 },
         { img: "/assets/jackson.jpg", img1: "/assets/sharky.png", img2: "/assets/lizardman.png", img3: "/assets/kitty.png", name: 'Jackson', level: 150, petAmount: 36, questAmount: 75 },
-        { img: "/assets/eric.jpg", img1: "/assets/frogman.jpg", img2: "/assets/waterlizard.png", img3: "/assets/giraffeboy.png", name: 'Eric', level: 99, petAmount: 28, questAmount: 35 },
+        { img: "/assets/eric.jpg", img1: "/assets/frogman.jpg", img2: "/assets/waterlizard.png", img3: "/assets/giraffeboy.png", name: 'Eric', level: 99, petAmount: 28, questAmount: 65 },
         { img: "/assets/jerry.jpg", img1: "/assets/chickboy.jpg", img2: "/assets/sharkchad.png", img3: "/assets/giraffechad.png", name: 'Jerry', level: 1, petAmount: 3, questAmount: 1 },
         { img: "/assets/frogchad.jpg", img1: "/assets/sheepchad.png", img2: "/assets/sharkchad.png", img3: "/assets/giraffechad.png", name: 'Poopman', level: 500, petAmount: 80, questAmount: 120 },
+        { img: "/assets/frogchad.jpg", img1: "/assets/sheepchad.png", img2: "/assets/sharkchad.png", img3: "/assets/giraffechad.png", name: 'Kj', level: 500, petAmount: 80, questAmount: 120 },
+    ];
+
+    const global = [
+        { img: "/assets/angella.png", img1: "/assets/monkeychad.png", img2: "/assets/whalechad.png", img3: "/assets/pangolin.png", name: 'Angella', level: 999, petAmount: 90, questAmount: 510 },
+        { img: "/assets/richard.png", img1: "/assets/frogchad.jpg", img2: "/assets/hedgehogking.png", img3: "/assets/fairysnake.png", name: 'Richard', level: 999, petAmount: 90, questAmount: 509 },
+        { img: "/assets/nicole.jpg", img1: "/assets/duckchad.png", img2: "/assets/sheepchad.png", img3: "/assets/whalechad.png", name: 'Nicole', level: 999, petAmount: 90, questAmount: 520 },
+        { img: "/assets/lebronsunshine.jpg", img1: "/assets/monkeybron.png", img2: "/assets/giraffechad.png", img3: "/assets/giraffeboy.png", name: 'lebron', level: 99, petAmount: 28, questAmount: 35 },
+        { img: "/assets/mexican.png", img1: "/assets/chickboy.jpg", img2: "/assets/sharkchad.png", img3: "/assets/giraffechad.png", name: 'lehbron', level: 1, petAmount: 3, questAmount: 1 },
+        { img: "/assets/fortnite.png", img1: "/assets/sheepchad.png", img2: "/assets/sharkchad.png", img3: "/assets/giraffechad.png", name: 'leGamer', level: 500, petAmount: 80, questAmount: 120 },
         { img: "/assets/frogchad.jpg", img1: "/assets/sheepchad.png", img2: "/assets/sharkchad.png", img3: "/assets/giraffechad.png", name: 'Kj', level: 500, petAmount: 80, questAmount: 120 },
     ];
     
     return (
         <div className="w-screen h-screen bg-gradient-to-t from-blue-800 to-green-300">
             <div className='py-8 pb-4 border-b-1'>
-                <p className='text-4xl font-bold text-white flex justify-center'>Leaderboard</p>
+                <div className='flex flex-col justify-center gap-4'>
+                    <PersonAddIcon className='flex justify-end ml-[89%] absolute text-white scale-[1.5]' />
+                    <p className='text-4xl font-bold text-white flex justify-center'>Leaderboard </p>
+                </div>
                 <div className='flex flex-row w-full justify-center gap-4'>
-                    <p className='text-white text-2xl underline'>friends</p>
+                    <button
+                        className={`text-2xl ${tab === 'friends' ? 'text-white underline' : 'text-[#E3E3E3]'}`}
+                        onClick={() => setTab('friends')}
+                    >
+                        friends
+                    </button>
                     <p className='text-white text-2xl'>|</p>
-                    <p className='text-[#E3E3E3] text-2xl'>global</p>
+                    <button
+                        className={`text-2xl ${tab === 'global' ? 'text-white underline' : 'text-[#E3E3E3]'}`}
+                        onClick={() => setTab('global')}
+                    >
+                        global
+                    </button>
                 </div>
             </div>
             <div className='bg-white py-2 h-full rounded-t-[30px] overflow-y-auto'>
-                {players.map((player, index) => (
-                    <PlayerCard
-                    key={index}
-                    img={player.img}
-                    img1={player.img1}
-                    img2={player.img2}
-                    img3={player.img3}
-                    name={player.name}
-                    level={player.level}
-                    petAmount={player.petAmount}
-                    questAmount={player.questAmount}
-                    delay={`${index * 0.2}`}
-                    />
-                ))}
-                {/* <PlayerCard img="/assets/frogchad.jpg" name='Kj' level='500' petAmount={80} questAmount={120} />
-                <PlayerCard img="/assets/frogmanner.jpg" name='Jerry' level='200' petAmount={42} questAmount={23} />
-                <PlayerCard img="/assets/frogman.jpg" name='Jackson' level='10' petAmount={23} questAmount={10} />
-                <PlayerCard img="/assets/chickboy.jpg" name='Eric' level='2' petAmount={3} questAmount={3} />
-                <PlayerCard img="/assets/kitty.png" name='Andrew' level='1' petAmount={2} questAmount={1} />
-                <PlayerCard img="/assets/kitty.png" name='Andrew' level='1' petAmount={2} questAmount={1} />
-                <PlayerCard img="/assets/kitty.png" name='Andrew' level='1' petAmount={2} questAmount={1} />
-                <PlayerCard img="/assets/kitty.png" name='Andrew' level='1' petAmount={2} questAmount={1} /> */}
+                {tab === 'friends' && (
+                    <div className='visible'>
+                        {friends.map((player, index) => (
+                            <PlayerCard
+                                key={index}
+                                img={player.img}
+                                img1={player.img1}
+                                img2={player.img2}
+                                img3={player.img3}
+                                name={player.name}
+                                level={player.level}
+                                petAmount={player.petAmount}
+                                questAmount={player.questAmount}
+                                delay={`${index * 0.2}s`}
+                            />
+                        ))}
+                    </div>
+                )}
+                {tab === 'global' && (
+                    <div className='visible'>
+                        {global.map((player, index) => (
+                            <PlayerCard
+                                key={index}
+                                img={player.img}
+                                img1={player.img1}
+                                img2={player.img2}
+                                img3={player.img3}
+                                name={player.name}
+                                level={player.level}
+                                petAmount={player.petAmount}
+                                questAmount={player.questAmount}
+                                delay={`${index * 0.2}s`}
+                            />
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     )
--- client/src/components/PlayerCard.tsx
+++ client/src/components/PlayerCard.tsx
@@ -15,19 +15,19 @@ interface PlayerCardProps {
 
 const PlayerCard: React.FC<PlayerCardProps> = ({img, img1, img2, img3, name, level, petAmount, questAmount, delay}) => {   
     return (
-        <div className={`w-[90%] my-4 mx-6 flex justify-center bg-[#ffffff] border-2 border-black rounded-2xl animate-slideRight transform -translate-x-full ease-in-out shadow-xl}`}
+        <div className={`w-[90%] my-4 mx-6 flex justify-center bg-[#1CB87C] border-l-[4px] border-r-[4px] border-t-[4px] border-[#276e53] rounded-2xl animate-slideRight transform -translate-x-full ease-in-out shadow-xl}`}
         style={{ animationDelay: `${delay}s` }}>
             <div className='flex w-full flex-row gap-4'>
-                <div className='bg-[#1CB87C] bg-gradient-to-t from-green-600 to-green-300 p-2 py-8 flex rounded-l-2xl border-black border-r-2'>
-                    <Image src={img} alt="Frogman" width={100} height={100} className='flex justify-center items-center border-2 border-black rounded-full'></Image>
+                <div className='bg-[#1CB87C]  p-2 py-8 flex rounded-l-2xl border-black'>
+                    <Image src={img} alt="Frogman" width={100} height={100} className='flex justify-center items-center border-[1px] border-black rounded-full'></Image>
                 </div>
                 <div className='flex flex-[3] pb-4 flex-col w-full'>
                     <div className='flex flex-row w-full py-1 justify-center'>
                         <p className='text-2xl flex-1 flex justify-items-start text-black'>{name}</p>
                         <p className='text-2xl flex-1 flex justify-center text-black'>Lvl {level}</p>
                     </div>
-                    <div className='flex flex-col mr-5 bg-[#1CB87C] bg-gradient-to-t from-green-600 to-green-300 border-2 border-black rounded-xl'>
-                        <div className='flex flex-row py-2 w-full border-b-2 border-black gap-2'>
+                    <div className='flex flex-col mr-5 bg-[#1CB87C] bg-gradient-to-t from-green-600 to-green-300 border-[1.5px] border-black rounded-xl'>
+                        <div className='flex flex-row py-2 w-full border-b-[1px] border-black gap-2'>
                             <div className='flex flex-col flex-0.5'>
                                 <p className='flex flex-[0.5] text-2xl text-white px-2'>Pets:</p>
                                 <p className='flex flex-[0.5] text-white px-2'>{petAmount}/90</p>
